877a58caab2b7c58ad3fc21b3af2cb6a8b1aa7f8
enyaaad <78340117+enyaaad@users.noreply.github.com>
2021-06-16 10:36:43 +0700

    made calculator

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -29,188 +29,198 @@ namespace convertionsystemnumbers
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
-			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
-			this.textBox2 = new System.Windows.Forms.TextBox();
-			this.textBox4 = new System.Windows.Forms.TextBox();
-			this.button1 = new System.Windows.Forms.Button();
-			this.label1 = new System.Windows.Forms.Label();
-			this.label2 = new System.Windows.Forms.Label();
-			this.label3 = new System.Windows.Forms.Label();
-			this.label4 = new System.Windows.Forms.Label();
-			this.label5 = new System.Windows.Forms.Label();
-			this.label6 = new System.Windows.Forms.Label();
-			this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
-			this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
-			this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
-			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
-			this.SuspendLayout();
-			// 
-			// textBox2
-			// 
-			this.textBox2.Location = new System.Drawing.Point(528, 153);
-			this.textBox2.Name = "textBox2";
-			this.textBox2.Size = new System.Drawing.Size(130, 23);
-			this.textBox2.TabIndex = 1;
-			// 
-			// textBox4
-			// 
-			this.textBox4.Location = new System.Drawing.Point(424, 251);
-			this.textBox4.Name = "textBox4";
-			this.textBox4.ReadOnly = true;
-			this.textBox4.Size = new System.Drawing.Size(100, 23);
-			this.textBox4.TabIndex = 3;
-			// 
-			// button1
-			// 
-			this.button1.BackColor = System.Drawing.SystemColors.ControlDark;
-			this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
-			this.button1.Location = new System.Drawing.Point(408, 314);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(137, 49);
-			this.button1.TabIndex = 4;
-			this.button1.Text = "игорь крутой";
-			this.button1.UseVisualStyleBackColor = false;
-			this.button1.Click += new System.EventHandler(this.button1_Click);
-			// 
-			// label1
-			// 
-			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(448, 68);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(0, 15);
-			this.label1.TabIndex = 5;
-			// 
-			// label2
-			// 
-			this.label2.AutoSize = true;
-			this.label2.Location = new System.Drawing.Point(389, 53);
-			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(185, 15);
-			this.label2.TabIndex = 6;
-			this.label2.Text = "ПЕРЕВОД СИСТЕМ СЧИСЛЕНИЯ\r\n";
-			// 
-			// label3
-			// 
-			this.label3.AutoSize = true;
-			this.label3.BackColor = System.Drawing.SystemColors.Desktop;
-			this.label3.ForeColor = System.Drawing.SystemColors.ControlLight;
-			this.label3.Location = new System.Drawing.Point(243, 123);
-			this.label3.Name = "label3";
-			this.label3.Size = new System.Drawing.Size(79, 15);
-			this.label3.TabIndex = 7;
-			this.label3.Text = "Исходная СС";
-			// 
-			// label4
-			// 
-			this.label4.AutoSize = true;
-			this.label4.BackColor = System.Drawing.SystemColors.Desktop;
-			this.label4.ForeColor = System.Drawing.SystemColors.Control;
-			this.label4.Location = new System.Drawing.Point(379, 124);
-			this.label4.Name = "label4";
-			this.label4.Size = new System.Drawing.Size(77, 15);
-			this.label4.TabIndex = 8;
-			this.label4.Text = "Итоговая СС";
-			// 
-			// label5
-			// 
-			this.label5.AutoSize = true;
-			this.label5.BackColor = System.Drawing.SystemColors.Desktop;
-			this.label5.ForeColor = System.Drawing.SystemColors.Info;
-			this.label5.Location = new System.Drawing.Point(528, 123);
-			this.label5.Name = "label5";
-			this.label5.Size = new System.Drawing.Size(98, 15);
-			this.label5.TabIndex = 9;
-			this.label5.Text = "Исходное число";
-			// 
-			// label6
-			// 
-			this.label6.AutoSize = true;
-			this.label6.BackColor = System.Drawing.SystemColors.Desktop;
-			this.label6.ForeColor = System.Drawing.SystemColors.Control;
-			this.label6.Location = new System.Drawing.Point(426, 233);
-			this.label6.Name = "label6";
-			this.label6.Size = new System.Drawing.Size(96, 15);
-			this.label6.TabIndex = 10;
-			this.label6.Text = "Итоговое число";
-			// 
-			// numericUpDown1
-			// 
-			this.numericUpDown1.Location = new System.Drawing.Point(243, 152);
-			this.numericUpDown1.Maximum = new decimal(new int[] {
+            this.components = new System.ComponentModel.Container();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(528, 153);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(130, 23);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(424, 251);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(100, 23);
+            this.textBox4.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(408, 314);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(137, 49);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Translate";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(448, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 15);
+            this.label1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(379, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(144, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Number system translator";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.Desktop;
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label3.Location = new System.Drawing.Point(243, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "First NS";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.Desktop;
+            this.label4.ForeColor = System.Drawing.SystemColors.Control;
+            this.label4.Location = new System.Drawing.Point(379, 124);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 15);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Second NS";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.Desktop;
+            this.label5.ForeColor = System.Drawing.SystemColors.Info;
+            this.label5.Location = new System.Drawing.Point(528, 123);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(85, 15);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Origin number";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.Desktop;
+            this.label6.ForeColor = System.Drawing.SystemColors.Control;
+            this.label6.Location = new System.Drawing.Point(426, 233);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(39, 15);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Result";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(243, 152);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
             36,
             0,
             0,
             0});
-			this.numericUpDown1.Minimum = new decimal(new int[] {
+            this.numericUpDown1.Minimum = new decimal(new int[] {
             2,
             0,
             0,
             0});
-			this.numericUpDown1.Name = "numericUpDown1";
-			this.numericUpDown1.Size = new System.Drawing.Size(120, 23);
-			this.numericUpDown1.TabIndex = 11;
-			this.numericUpDown1.Value = new decimal(new int[] {
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 23);
+            this.numericUpDown1.TabIndex = 11;
+            this.numericUpDown1.Value = new decimal(new int[] {
             2,
             0,
             0,
             0});
-			// 
-			// numericUpDown2
-			// 
-			this.numericUpDown2.Location = new System.Drawing.Point(379, 153);
-			this.numericUpDown2.Maximum = new decimal(new int[] {
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(379, 153);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
             36,
             0,
             0,
             0});
-			this.numericUpDown2.Minimum = new decimal(new int[] {
+            this.numericUpDown2.Minimum = new decimal(new int[] {
             2,
             0,
             0,
             0});
-			this.numericUpDown2.Name = "numericUpDown2";
-			this.numericUpDown2.Size = new System.Drawing.Size(120, 23);
-			this.numericUpDown2.TabIndex = 12;
-			this.numericUpDown2.Value = new decimal(new int[] {
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 23);
+            this.numericUpDown2.TabIndex = 12;
+            this.numericUpDown2.Value = new decimal(new int[] {
             2,
             0,
             0,
             0});
-			// 
-			// notifyIcon1
-			// 
-			this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Warning;
-			this.notifyIcon1.Text = "Выберите значение \r\n";
-			this.notifyIcon1.Visible = true;
-			// 
-			// MainForm
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-			this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-			this.ClientSize = new System.Drawing.Size(997, 538);
-			this.Controls.Add(this.numericUpDown2);
-			this.Controls.Add(this.numericUpDown1);
-			this.Controls.Add(this.label6);
-			this.Controls.Add(this.label5);
-			this.Controls.Add(this.label4);
-			this.Controls.Add(this.label3);
-			this.Controls.Add(this.label2);
-			this.Controls.Add(this.label1);
-			this.Controls.Add(this.button1);
-			this.Controls.Add(this.textBox4);
-			this.Controls.Add(this.textBox2);
-			this.DoubleBuffered = true;
-			this.Name = "MainForm";
-			this.Text = "SysNum";
-			this.Load += new System.EventHandler(this.Form1_Load);
-			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Warning;
+            this.notifyIcon1.Text = "Выберите значение \r\n";
+            this.notifyIcon1.Visible = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(243, 314);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(134, 49);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Calculator of numbers";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(997, 538);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.DoubleBuffered = true;
+            this.Name = "MainForm";
+            this.Text = "SysNum";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 
@@ -227,6 +237,7 @@ namespace convertionsystemnumbers
 		private System.Windows.Forms.NumericUpDown numericUpDown1;
 		private System.Windows.Forms.NumericUpDown numericUpDown2;
 		public System.Windows.Forms.NotifyIcon notifyIcon1;
-	}
+        private System.Windows.Forms.Button button2;
+    }
 }
 
---
+++ Math.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ConvertSN;
+
+namespace convertionsystemnumbers
+{
+    public partial class Math : Form
+    {
+        public Math()
+        {
+            InitializeComponent();
+        }
+
+        private void Math_Load(object sender, EventArgs e)
+        {
+
+        }
+        private static int Error(string number, int xd, int currentns)
+        {
+            for (int i = 0; i < number.Length; i++)
+            {
+                xd = ConvertNS.CharToInt(number[i]);
+                if (xd >= currentns)
+                {
+                    return -1;
+                }
+
+            }
+            if (number == "" || number == " ")
+            {
+                return -1;
+            }
+
+            return 0;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string numberfirst = "";
+            string numbersecond = "";
+            int numsys = 0;
+            int xd = 0, errorcheck = 0;
+            int firstresult = 0;
+            string temps1, temps2, temps3 ;
+            try
+            {
+                textBox4.Text = "";
+                numberfirst = textBox1.Text;
+                numbersecond = textBox2.Text;
+                numsys = Convert.ToInt32(numericUpDown1.Value);
+                errorcheck = Error(numberfirst, xd, numsys);
+
+            }
+            catch
+            {
+                MessageBox.Show("Enter Valid Value");
+            }
+            if (errorcheck == -1)
+            {
+                MessageBox.Show("Enter Valid Value");
+            }
+            else
+            {
+                try
+                {
+                    string comboboxstate = comboBox1.Text;
+                    if (comboBox1.Text == "Сложение")
+                    {
+                        temps1 = ConvertNS.Converting(numsys, 10, numberfirst);
+                        temps2 = ConvertNS.Converting(numsys, 10, numbersecond);
+                        firstresult = Convert.ToInt32(temps1) + Convert.ToInt32(temps2);
+                    }
+                    else if (comboBox1.Text == "Вычитание")
+                    {
+                        temps1 = ConvertNS.Converting(numsys, 10, numberfirst);
+                        temps2 = ConvertNS.Converting(numsys, 10, numbersecond);
+                        firstresult = Convert.ToInt32(temps1) - Convert.ToInt32(temps2);
+                        if (Convert.ToInt32(temps1) < Convert.ToInt32(temps2))
+                        {
+                            MessageBox.Show("we don't work with negative numbers yet");
+
+                        }
+                        temps3 = ConvertNS.Converting(10, numsys, firstresult.ToString());
+                        if (Convert.ToInt32(temps3) > 1000000)
+                            MessageBox.Show("Too Big Value to display");
+                    }             
+                    else
+                    {
+                        MessageBox.Show("Enter Valid Action");
+                    }
+                    textBox4.Text = ConvertNS.Converting(10, numsys, firstresult.ToString());
+
+                }
+                catch
+                {
+                    MessageBox.Show("Error while counting ");
+                }
+            }
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            MainForm Mainf = new MainForm();
+            Mainf.ShowDialog();
+        }
+    }
+}
